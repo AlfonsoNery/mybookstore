310a8ea93f7c36a8d144d048610eea177cbd516f
11602075 <11602075@TAFT-CL320>
2017-09-15 12:11:56 +0800

    initial commit

---
+++ mybookstore/App_Code/joseph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+
+namespace mybookstore.App_Code
+{
+    public class joseph
+    {
+        public static string GetCon()
+        {
+            return ConfigurationManager.ConnectionStrings["MyCon"].ConnectionString;
+        }
+
+    }
+}
